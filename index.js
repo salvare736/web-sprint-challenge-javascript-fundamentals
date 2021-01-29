321da0930aede7e3243ee9c54c1690b007370232
Sergio Alvarez <salvare736@gmail.com>
2021-01-29 11:43:25 -0500

    Task 2 request 3 completed

--- index.js
+++ index.js
@@ -90,10 +90,14 @@ const zooAnimals = [
   Using lowPopulationAnimals use .filter() to create a new array of objects which contains only the animals with a population of less than 5.
   */
 
-  function lowPopulationAnimals(/*Your Code Here*/){
-    /*Your Code Here*/
+  function lowPopulationAnimals(array){
+    const endangeredList = array.filter(function(item){
+      return item.population < 5;
+    });
+    return endangeredList;
   }
   
+  // console.log(lowPopulationAnimals(zooAnimals));
 
   /* 🦁🦁🦁 Request 4: .reduce() 🦁🦁🦁
   The zoo needs to know their total animal population across the United States. 
